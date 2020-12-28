e63c8ef9410a6198d040d815e7bd1f45e17e1574
amandeepmittal <amandeepmittal@live.com>
2020-12-28 20:19:05 +0530

    add 2 screen components

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 // import screens
 import Listings from '../screens/App/Listings';
+import Favorites from '../screens/App/Favorites';
+import Profile from '../screens/App/Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +12,8 @@ const AppNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Listings" component={Listings} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 };
