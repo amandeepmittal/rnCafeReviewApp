6bd4c5664a355b8f1f60059e0dfc5ab5f44ae4c5
amandeepmittal <amandeepmittal@live.com>
2020-12-29 19:09:06 +0530

    customize tab bar

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
 
 // import screens
 import Listings from '../screens/App/Listings';
@@ -10,7 +11,36 @@ const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        activeTintColor: '#cc7351',
+        inactiveTintColor: '#d7cbb4',
+        style: {
+          backgroundColor: '#fff',
+        },
+      }}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color}) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'Listings':
+              iconName = 'coffee';
+              break;
+            case 'Favorites':
+              iconName = 'heart';
+              break;
+            case 'Profile':
+              iconName = 'user';
+              break;
+            default:
+              break;
+          }
+
+          return <Feather name={iconName} color={color} size={24} />;
+        },
+      })}>
       <Tab.Screen name="Listings" component={Listings} />
       <Tab.Screen name="Favorites" component={Favorites} />
       <Tab.Screen name="Profile" component={Profile} />
